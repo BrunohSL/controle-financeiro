405787b17d4b2765fa41d56d50d68fce9ccf78b6
BrunohSL <brunu1994@gmail.com>
2025-06-03 16:34:12 -0300

    Rotas de contas + refactor auth

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,10 @@ use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 // Auth
-Route::get('/login', [AuthController::class, 'auth']);
+Route::get('/auth', [AuthController::class, 'auth']);
 
 // Accounts
-Route::get('/accounts/show-accounts', [AccountController::class, 'show']);
-Route::get('/accounts/create-account', [AccountController::class, 'create']);
+Route::get('accounts/show-accounts', [AccountController::class, 'show']);
+Route::post('accounts/create-account', [AccountController::class, 'create']);
+Route::put('accounts/update-account', [AccountController::class, 'update']);
+Route::delete('accounts/delete-account', [AccountController::class, 'kill']);
